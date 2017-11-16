301b65e0c0335709dfce89ed650349b1fec64462
Dom Kapelewski <info@redfrogstudio.co.uk>
2017-11-16 08:21:01 +0000

    added obj_clean() for the footer logo

--- wp-content/themes/kickstarter/src/functions/shortcodes/logo.php
+++ wp-content/themes/kickstarter/src/functions/shortcodes/logo.php
@@ -10,5 +10,8 @@ add_filter('widget_text','do_shortcode');
 add_shortcode('logo', 'logo_shortcode_function');
 
 function logo_shortcode_function() {
-  return main_logo(false, 55, 312);
+  ob_start();
+  main_logo(false, 55, 312);
+  $content = ob_get_clean();
+  return $content;
 }
