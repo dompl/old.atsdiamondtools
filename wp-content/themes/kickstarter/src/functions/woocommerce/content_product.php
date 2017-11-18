d12c089d9ed4d019d4a797f7115afec7a2130dc2
Dom Kapelewski <info@redfrogstudio.co.uk>
2017-11-18 07:26:00 +0000

    Removed ajax from add to cart

--- wp-content/themes/kickstarter/src/functions/woocommerce/content_product.php
+++ wp-content/themes/kickstarter/src/functions/woocommerce/content_product.php
@@ -41,10 +41,10 @@ function ats_create_product_listing_layout_()
 
   // Product image
   $prod .= sprintf('
-  <div class="product-list-image">
+    <div class="product-list-image">
     <a href="%s" title="%s">%s</a>
-  </div>
-  ',
+    </div>
+    ',
     $prod_url, // Product URL
     esc_html($prod_title),
     $prod_image
@@ -55,7 +55,7 @@ function ats_create_product_listing_layout_()
   // Product
   $prod .= sprintf('
     <div class="product-list-title">
-      <a href="%s" title="%s">%s</a>
+    <a href="%s" title="%s">%s</a>
     </div>
     <div class="product-list-short">
     %s
@@ -72,7 +72,7 @@ function ats_create_product_listing_layout_()
   $prod .= '</div>'; // End content container
 
   // Add to cart
-  $add_to_cart = sprintf('<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="button product_type_%s ajax_add_to_cart add_to_cart_button">%s</a>',
+  $add_to_cart = sprintf('<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="button product_type_%s">%s</a>',
     esc_url($product->add_to_cart_url()),
     esc_attr($id),
     esc_attr($product->get_sku()),
@@ -91,11 +91,11 @@ function ats_create_product_listing_layout_()
   }
   $cat_list .= '</ul>';
 
-   $prod .= sprintf('
+  $prod .= sprintf('
     <div class="product-content-colleterals clx">
-      <div class="product-list-cats">%s</div>
-      <div class="product-list-price">%s</div>
-      <div class="product-list-add-cart">%s</div>
+    <div class="product-list-cats">%s</div>
+    <div class="product-list-price">%s</div>
+    <div class="product-list-add-cart">%s</div>
     </div>
     ',
     $cat_list,
