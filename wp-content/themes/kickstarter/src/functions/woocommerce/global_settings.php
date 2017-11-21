4d7b3b7f62702e421900bc1ffebe1642158a3b90
Dom Kapelewski <info@redfrogstudio.co.uk>
2017-11-21 06:36:59 +0000

    changed number pf products

--- wp-content/themes/kickstarter/src/functions/woocommerce/global_settings.php
+++ wp-content/themes/kickstarter/src/functions/woocommerce/global_settings.php
@@ -10,3 +10,14 @@ add_theme_support('woocommerce');
 /* Remove all stylesheets */
 
 add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
+/* Change the amount of products displayed on listing */
+
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
+
+function new_loop_shop_per_page( $cols ) {
+  // $cols contains the current number of products per page based on the value stored on Options -> Reading
+  // Return the number of products you wanna show per page.
+  $cols = -1;
+  return $cols;
+}
