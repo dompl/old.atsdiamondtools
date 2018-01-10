0a356047eaf8c2a02536ace4bd8606f02a0913a9
Dom Kapelewski <info@redfrogstudio.co.uk>
2018-01-10 07:53:58 +0000

    404 page

--- wp-content/themes/kickstarter/src/404.php
+++ wp-content/themes/kickstarter/src/404.php
@@ -1,12 +1,14 @@
-<?php get_header(); ?>
+<?php get_header();?>
 <main>
-  <article id="404">
+  <article class="container" id="page-404">
     <div class="image_404">
-      <img src="<?php echo get_template_directory_uri(); ?>/img/logos/404.png" alt="<?php esc_html_e( 'Page not faund', TEXT_DOMAIN) ?>">
+      <a href="<?php echo esc_url(home_url()) ?>">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/theme/404.png" alt="<?php esc_html_e('Page not faund', TEXT_DOMAIN)?>">
+      </a>
     </div>
-    <p><?php esc_html_e( 'We\'re Sorry...', TEXT_DOMAIN) ?></p>
-    <p><?php esc_html_e( 'We can\'t seem to find the page you are looking for', TEXT_DOMAIN) ?></p>
-    <a href="<?php bloginfo('url'); ?>"><?php esc_html_e( 'Return to home page', TEXT_DOMAIN) ?></a>
+    <p><?php esc_html_e('We\'re Sorry...', TEXT_DOMAIN)?></p>
+    <p><?php esc_html_e('We can\'t seem to find the page you are looking for', TEXT_DOMAIN)?></p>
+    <a href="<?php echo esc_url(home_url()) ?>" class="button"><?php esc_html_e('Return to home page', TEXT_DOMAIN)?></a>
   </article>
 </main>
 <?php get_footer();
