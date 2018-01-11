8f0aa7fde51d3e656119645dfac772c4bfc9e153
Matt <matt@redfrogstudio.uk>
2018-01-11 10:35:37 +0000

    add word menu to mobile nav

--- wp-content/themes/kickstarter/src/modules/header/navigation.php
+++ wp-content/themes/kickstarter/src/modules/header/navigation.php
@@ -3,7 +3,8 @@
     <nav id="navigation" class="navigation navigation-landscape" data-breakpoint="768">
      <div class="nav-header">
       <?php echo main_logo(false, 30, 170); ?>
-      <div class="nav-toggle"></div>
+
+      <div class="nav-toggle"><span><?php _e('Menu', 'TEXT_DOMAIN')?></span></div>
     </div>
     <div class="nav-menus-wrapper">
      <?php wp_nav_menu(array('theme_location' => 'main', 'menu_id' => 'menu-header', 'menu_class' => 'nav-menu align-to-left'));?>
