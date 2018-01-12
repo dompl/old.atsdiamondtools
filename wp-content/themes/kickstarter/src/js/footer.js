406dc56bf7304ac82ee340cf5611700b423d3bcf
Matt <matt@redfrogstudio.uk>
2018-01-12 11:29:56 +0000

    add checkPAssStrenght function

--- wp-content/themes/kickstarter/src/js/footer.js
+++ wp-content/themes/kickstarter/src/js/footer.js
@@ -2,6 +2,82 @@
 ;
 (function($) {
 		$(function() {
+
+				function checkPasswordStrength($pass1,
+						$pass2,
+						$strengthResult,
+						$submitButton,
+						blacklistArray) {
+						var pass1 = $pass1.val();
+						var pass2 = $pass2.val();
+
+						// Reset the form & meter
+						$submitButton.attr('disabled', 'disabled');
+						$strengthResult.removeClass('short bad good strong');
+
+						// Extend our blacklist array with those from the inputs & site data
+						blacklistArray = blacklistArray.concat(wp.passwordStrength.userInputBlacklist())
+
+						// Get the password strength
+						var strength = wp.passwordStrength.meter(pass1, blacklistArray, pass2);
+						// Add the strength meter results
+						switch (strength) {
+
+								case 2:
+										$strengthResult.addClass('bad').html(pwsL10n.bad);
+										$('.pw-weak').css('display', 'block');
+										$submitButton.attr('disabled', 'disabled');
+										break;
+
+								case 3:
+										$strengthResult.addClass('good').html(pwsL10n.good);
+										$('.pw-weak').css('display', 'none');
+										$submitButton.removeAttr('disabled');
+										break;
+
+								case 4:
+										$strengthResult.addClass('strong').html(pwsL10n.strong);
+										$('.pw-weak').css('display', 'none');
+										$submitButton.removeAttr('disabled');
+										break;
+
+								case 5:
+										$strengthResult.addClass('short').html(pwsL10n.mismatch);
+										$('.pw-weak').css('display', 'block');
+										$submitButton.attr('disabled', 'disabled');
+										break;
+
+								default:
+										$strengthResult.addClass('short').html(pwsL10n.short);
+						}
+
+						// The meter function returns a result even if pass2 is empty,
+						// enable only the submit button if the password is strong and
+						// both passwords are filled up
+
+						return strength;
+				}
+
+				jQuery(document).ready(function($) {
+						$('#customer_login .register').on('keyup', 'input[name=password], input[name=password_retyped]', function(event) {
+								checkPasswordStrength(
+										$('#reg_password'), // First password field
+										$('input[name=password_retyped]'), // Second password field
+										$('#password-strength'), // Strength meter
+										$('input[type=submit]'), // Submit button
+										['black', 'listed', 'word'] // Blacklisted words
+								);
+						});
+				});
+
+				$('.pw-weak input').click(function() {
+						if (this.checked === true) {
+								$('.register input[type=submit]').removeAttr('disabled');
+						} else {
+								$('.register input[type=submit]').attr('disabled', 'disabled');
+						}
+				});
+
 				/*  ********************************************************
 				 *   Search Show : Hide
 				 *  ********************************************************
