038aa19f9abcd32c61e655baac9d9e331d6f29d1
Dom Kapelewski <info@redfrogstudio.co.uk>
2018-01-17 08:07:25 +0000

    Fix - Variable price.
    Prodcut was not showing the price if variation prices were the same.

--- wp-content/themes/kickstarter/src/functions/woocommerce/single/price.php
+++ wp-content/themes/kickstarter/src/functions/woocommerce/single/price.php
@@ -4,29 +4,47 @@
  *  ********************************************************
  */
 
-function custom_wc_template_single_price(){
+function custom_wc_template_single_price()
+{
   global $product;
 
   // Variable product only
-  if($product->is_type('variable')):
-
+  if ($product->is_type('variable')):
 
     $suffix = $product->is_taxable() ? get_option('woocommerce_price_display_suffix') : '';
 
     // Main Price
-    $prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
-    $price = $prices[0] !== $prices[1] ? sprintf( __( 'From: %1$s %2$s', 'woocommerce' ), wc_price( $prices[0] ), $suffix ) : wc_price( $prices[0] );
+    $prices = array($product->get_variation_price('min', true), $product->get_variation_price('max', true));
+    $price  = $prices[0] !== $prices[1] ? sprintf(__('From: %1$s %2$s', 'woocommerce'), wc_price($prices[0]), $suffix) : wc_price($prices[0]);
     // Sale Price
-    $prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
-    sort( $prices );
-    $saleprice = $prices[0] !== $prices[1] ? sprintf( __( 'From: %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+    $prices = array($product->get_variation_regular_price('min', true), $product->get_variation_regular_price('max', true));
+    sort($prices);
+    $saleprice = $prices[0] !== $prices[1] ? sprintf(__('From: %1$s', 'woocommerce'), wc_price($prices[0])) : wc_price($prices[0]);
 
-    if ( $price !== $saleprice && $product->is_on_sale() ) {
+    if ($price !== $saleprice && $product->is_on_sale())
+  {
       $price = '<del>' . $saleprice . $product->get_price_suffix() . '</del> <ins>' . $price . $product->get_price_suffix() . '</ins>';
     }
 
-    echo '<p class="price">'.$price.'</p><div class="hidden-variable-price" >'.$price.'</div>';
-  else :
+    $available_variations = $product->get_available_variations();
+
+    foreach ($available_variations as $value)
+  {
+      $variation_regural_prices[] = $value['display_regular_price'];
+    }
+
+    if (count(array_unique($variation_regural_prices)) <= 1)
+  {
+       echo '<p class="price-a">' . $price . '</p>';
+    }
+  else
+  {
+      echo '<p class="price">' . $price . '</p><div class="hidden-variable-price" >' . $price . '</div>';
+    }
+
+
+
+  else:
     echo $product->get_price_html();
   endif;
 }
