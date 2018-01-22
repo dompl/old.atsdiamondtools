24c0d98fd2a3dcba2a529132cb346a8c75cb84ec
Dom Kapelewski <info@redfrogstudio.co.uk>
2018-01-22 10:20:34 +0000

    added all files per new gitignore

--- wp-content/themes/atsdiamondtools/functions/woocommerce/ajax-search.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* ajax search = remove basic styles */
-add_action('wp_print_styles', 'mytheme_dequeue_css_from_plugins', 100);
-function mytheme_dequeue_css_from_plugins()
-{
-  wp_dequeue_style('aws-style');
-}
