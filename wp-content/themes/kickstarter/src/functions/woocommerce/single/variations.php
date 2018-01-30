a7acfaade918cd4ceae13727ec29f900ae169e1f
Dom Kapelewski <info@redfrogstudio.co.uk>
2018-01-30 10:15:30 +0000

    added stock quantity

--- wp-content/themes/kickstarter/src/functions/woocommerce/single/variations.php
+++ wp-content/themes/kickstarter/src/functions/woocommerce/single/variations.php
@@ -10,59 +10,72 @@ add_filter('woocommerce_dropdown_variation_attribute_options_html', 'ats_variati
 add_filter('woocommerce_variation_option_name', 'display_price_in_variation_option_name');
 
 /* Change Variation label */
-function ats_attribute_label($label, $name, $product) {
-    $name = __('Select Option', 'TEXT_DOMAIN');
-    if (is_cart() || is_checkout()) {
-        $name = __('Option', 'TEXT_DOMAIN');
-    }
-    return ($name);
+function ats_attribute_label($label, $name, $product)
+{
+  $name = __('Select Option', 'TEXT_DOMAIN');
+  if (is_cart() || is_checkout())
+  {
+    $name = __('Option', 'TEXT_DOMAIN');
+  }
+  return ($name);
 }
 
 /* Change variation reset link */
-function ats_change_clear_option() {
-    return '<a class="reset_variations clx" href="#"><i class="icon-times-circle"></i></a>';
+function ats_change_clear_option()
+{
+  return '<a class="reset_variations clx" href="#"><i class="icon-times-circle"></i></a>';
 }
 
 /* Change "No variation selected" in variation dropdown */
-function ats_variation_attribute_options_html($html, $args) {
-    $html = str_replace('Choose an option', __('-- No option selected --', 'TEXT_DOMAIN'), $html);
-    return $html;
+function ats_variation_attribute_options_html($html, $args)
+{
+  $html = str_replace('Choose an option', __('-- No option selected --', 'TEXT_DOMAIN'), $html);
+  return $html;
 }
 
 /* Add price to variation dropdown */
-function display_price_in_variation_option_name($term) {
+function display_price_in_variation_option_name($term)
+{
 
-    if (is_admin()) {
-        return $term;
-    }
+  if (is_admin())
+  {
+    return $term;
+  }
 
-    global $wpdb, $product;
+  global $wpdb, $product;
 
-    $result = $wpdb->get_col("SELECT slug FROM {$wpdb->prefix}terms WHERE name = '$term'");
+  $result = $wpdb->get_col("SELECT slug FROM {$wpdb->prefix}terms WHERE name = '$term'");
 
-    $term_slug   = ( ! empty($result)) ? $result[0] : $term;
-    $product__id = $product->get_id();
-    $query       = "SELECT postmeta.post_id AS product_id
+  $term_slug   = (!empty($result)) ? $result[0] : $term;
+  $product__id = $product->get_id();
+  $query       = "SELECT postmeta.post_id AS product_id
                   FROM {$wpdb->prefix}postmeta AS postmeta
                   LEFT JOIN {$wpdb->prefix}posts AS products ON ( products.ID = postmeta.post_id )
                   WHERE postmeta.meta_key LIKE 'attribute_%'
                   AND postmeta.meta_value = '$term_slug'
                   AND products.post_parent = $product__id";
 
-    $variation_id = $wpdb->get_col($query);
+  $variation_id = $wpdb->get_col($query);
 
-    $parent = wp_get_post_parent_id($variation_id[0]);
+  $parent = wp_get_post_parent_id($variation_id[0]);
 
-    if ($parent > 0) {
-        $_product = new WC_Product_Variation($variation_id[0]);
+  if ($parent > 0)
+  {
+    $_product = new WC_Product_Variation($variation_id[0]);
 
 // $is_tax = get_option('woocommerce_tax_display_shop');
+    $oos = '';
 
-        $tax_tag = get_option('woocommerce_price_display_suffix');
+    if ($_product->get_stock_status() === 'outofstock' || ($_product->get_manage_stock() === true && $_product->get_stock_quantity() < 1))
+    {
+      $oos = ' - ' . __('OUT OF STOCK', 'woocommerce');
+    }
+
+    $tax_tag = get_option('woocommerce_price_display_suffix');
 
 //this is where you can actually customize how the price is displayed
-        return $term . ' (' . strip_tags(wc_price($_product->get_price())) . $tax_tag . ')';
-    }
-    return $term;
+    return $term . ' (' . strip_tags(wc_price($_product->get_price())) . $tax_tag . ')' . $oos;
+  }
+  return $term;
 
 }
