54f10aebe8406744a12bbe110dc7e034e01f469b
Matt <matt@redfrogstudio.uk>
2018-02-05 13:18:12 +0000

    fix confir weak pass on login page

--- wp-content/themes/kickstarter/src/js/footer.js
+++ wp-content/themes/kickstarter/src/js/footer.js
@@ -1,189 +1,152 @@
 // ==== FOOTER ==== //
-;(function($) {
-		$(function() {
+;
+(function($) {
+    $(function() {
+        function checkPasswordStrength($pass1, $pass2, $strengthResult, $submitButton, blacklistArray) {
+            var pass1 = $pass1.val();
+            var pass2 = $pass2.val();
+            // Reset the form & meter
+            $submitButton.attr('disabled', 'disabled');
+            $strengthResult.removeClass('short bad good strong');
+            // Extend our blacklist array with those from the inputs & site data
+            blacklistArray = blacklistArray.concat(wp.passwordStrength.userInputBlacklist())
+            // Get the password strength
+            var strength = wp.passwordStrength.meter(pass1, blacklistArray, pass2);
+            // Add the strength meter results
+            if (strength < 3) {
+                $strengthResult.addClass('bad').html(pwsL10n.bad);
+                $('.pw-weak').css('display', 'block');
+                $submitButton.attr('disabled', 'disabled');
+            } else {
+                $strengthResult.addClass('strong').html(pwsL10n.strong);
+                $('.pw-weak').css('display', 'none');
+                $submitButton.removeAttr('disabled');
+            }
+            // The meter function returns a result even if pass2 is empty,
+            // enable only the submit button if the password is strong and
+            // both passwords are filled up
+            return strength;
+        }
+        jQuery(document).ready(function($) {
+            $('#customer_login .register').on('keyup', 'input[name=password], input[name=password_retyped]', function(event) {
+                checkPasswordStrength($('#reg_password'), // First password field
+                    $('input[name=password_retyped]'), // Second password field
+                    $('#password-strength'), // Strength meter
+                    $('input[type=submit]'), // Submit button
+                    ['black', 'listed', 'word'] // Blacklisted words
+                );
+            });
+        });
+        $('.pw-checkbox').on('click', function() {
+            $('.woocommerce-FormRow input[type="submit"]').removeAttr('disabled');
+        });
+        $('.pw-weak input').click(function() {
+            if (this.checked === true) {
+                $('.register input[type=submit]').removeAttr('disabled');
+            } else {
+                $('.register input[type=submit]').attr('disabled', 'disabled');
+            }
+        });
+        /*  ********************************************************
+         *   Search Show : Hide
+         *  ********************************************************
+         */
+        $('#reg_password').on('keyup', function() {
+            console.log(pwsL10n);
+        });
 
-				function checkPasswordStrength($pass1,
-						$pass2,
-						$strengthResult,
-						$submitButton,
-						blacklistArray) {
-						var pass1 = $pass1.val();
-						var pass2 = $pass2.val();
-
-						// Reset the form & meter
-						$submitButton.attr('disabled', 'disabled');
-						$strengthResult.removeClass('short bad good strong');
-
-						// Extend our blacklist array with those from the inputs & site data
-						blacklistArray = blacklistArray.concat(wp.passwordStrength.userInputBlacklist())
-
-						// Get the password strength
-						var strength = wp.passwordStrength.meter(pass1, blacklistArray, pass2);
-						// Add the strength meter results
-						switch (strength) {
-
-								case 2:
-										$strengthResult.addClass('bad').html(pwsL10n.bad);
-										$('.pw-weak').css('display', 'block');
-										$submitButton.attr('disabled', 'disabled');
-										break;
-
-								case 3:
-										$strengthResult.addClass('good').html(pwsL10n.good);
-										$('.pw-weak').css('display', 'none');
-										$submitButton.removeAttr('disabled');
-										break;
-
-								case 4:
-										$strengthResult.addClass('strong').html(pwsL10n.strong);
-										$('.pw-weak').css('display', 'none');
-										$submitButton.removeAttr('disabled');
-										break;
-
-								case 5:
-										$strengthResult.addClass('short').html(pwsL10n.mismatch);
-										$('.pw-weak').css('display', 'block');
-										$submitButton.attr('disabled', 'disabled');
-										break;
-
-								default:
-										$strengthResult.addClass('short').html(pwsL10n.short);
-						}
-
-						// The meter function returns a result even if pass2 is empty,
-						// enable only the submit button if the password is strong and
-						// both passwords are filled up
-
-						return strength;
-				}
-
-				jQuery(document).ready(function($) {
-						$('#customer_login .register').on('keyup', 'input[name=password], input[name=password_retyped]', function(event) {
-								checkPasswordStrength(
-										$('#reg_password'), // First password field
-										$('input[name=password_retyped]'), // Second password field
-										$('#password-strength'), // Strength meter
-										$('input[type=submit]'), // Submit button
-										['black', 'listed', 'word'] // Blacklisted words
-								);
-						});
-				});
-
-				$('.pw-checkbox').on('click', function() {
-					$('.woocommerce-FormRow input[type="submit"]').removeAttr('disabled');
-				});
-
-				$('.pw-weak input').click(function() {
-						if (this.checked === true) {
-								$('.register input[type=submit]').removeAttr('disabled');
-						} else {
-								$('.register input[type=submit]').attr('disabled', 'disabled');
-						}
-				});
-
-				/*  ********************************************************
-				 *   Search Show : Hide
-				 *  ********************************************************
-				 */
-
-
-				$('#reg_password').on('keyup', function() {
-						console.log(pwsL10n);
-				});
-
-
-				function searach_toggle() {
-						$('.search-toggle').click(function() {
-								$(".search-section").stop().slideToggle("fast");
-						});
-				}
-				searach_toggle();
-				/*  ********************************************************
-				 *   Match height general class
-				 *  ********************************************************
-				 */
-				$('.mh').matchHeight();
-				/*  ********************************************************
-				 *   Cookie Bar
-				 *   https://github.com/kiuz/jquery-cookie-bar/blob/master/index.html
-				 *  ********************************************************
-				 */
-				$.cookieBar({});
-				/*  ********************************************************
-				 *   Slick Carousel settings
-				 *  ********************************************************
-				 */
-				$('.home-products-container #products-list').slick({
-						dots: false,
-						infinite: true,
-						speed: 300,
-						arrows: true,
-						slidesToShow: 4,
-						slidesToScroll: 4,
-						prevArrow: '<i class="icon-arrow-thin-left"></i>',
-						nextArrow: '<i class="icon-arrow-thin-right"></i>',
-						responsive: [{
-								breakpoint: 1024,
-								settings: {
-										slidesToShow: 3,
-										slidesToScroll: 3,
-										infinite: true,
-										dots: true
-								}
-						}, {
-								breakpoint: 600,
-								settings: {
-										slidesToShow: 2,
-										slidesToScroll: 2
-								}
-						}, {
-								breakpoint: 480,
-								settings: {
-										slidesToShow: 1,
-										slidesToScroll: 1
-								}
-						}]
-				});
-				$('#home-slider-ul').slick({
-						dots: false,
-						infinite: true,
-						speed: 300,
-						arrows: true,
-						slidesToShow: 1,
-						slidesToScroll: 1,
-						prevArrow: '<i class="icon-arrow-thin-left"></i>',
-						nextArrow: '<i class="icon-arrow-thin-right"></i>',
-				});
-				$('#carousel').slick({
-						dots: true,
-						infinite: false,
-						speed: 300,
-						arrows: true,
-						slidesToShow: 4,
-						slidesToScroll: 4,
-						prevArrow: '<i class="icon-arrow-thin-left"></i>',
-						nextArrow: '<i class="icon-arrow-thin-right"></i>',
-						responsive: [{
-								breakpoint: 1024,
-								settings: {
-										slidesToShow: 3,
-										slidesToScroll: 3,
-										infinite: true,
-										dots: true
-								}
-						}, {
-								breakpoint: 600,
-								settings: {
-										slidesToShow: 2,
-										slidesToScroll: 2
-								}
-						}, {
-								breakpoint: 480,
-								settings: {
-										slidesToShow: 1,
-										slidesToScroll: 1
-								}
-						}]
-				});
-		});
+        function searach_toggle() {
+            $('.search-toggle').click(function() {
+                $(".search-section").stop().slideToggle("fast");
+            });
+        }
+        searach_toggle();
+        /*  ********************************************************
+         *   Match height general class
+         *  ********************************************************
+         */
+        $('.mh').matchHeight();
+        /*  ********************************************************
+         *   Cookie Bar
+         *   https://github.com/kiuz/jquery-cookie-bar/blob/master/index.html
+         *  ********************************************************
+         */
+        $.cookieBar({});
+        /*  ********************************************************
+         *   Slick Carousel settings
+         *  ********************************************************
+         */
+        $('.home-products-container #products-list').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            arrows: true,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            prevArrow: '<i class="icon-arrow-thin-left"></i>',
+            nextArrow: '<i class="icon-arrow-thin-right"></i>',
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
+        $('#home-slider-ul').slick({
+            dots: false,
+            infinite: true,
+            speed: 300,
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            prevArrow: '<i class="icon-arrow-thin-left"></i>',
+            nextArrow: '<i class="icon-arrow-thin-right"></i>',
+        });
+        $('#carousel').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            arrows: true,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            prevArrow: '<i class="icon-arrow-thin-left"></i>',
+            nextArrow: '<i class="icon-arrow-thin-right"></i>',
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            }, {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }]
+        });
+    });
 }(jQuery));
