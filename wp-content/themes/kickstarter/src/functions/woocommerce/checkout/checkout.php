eab7c9a418f22ae1b268a3f2101141c7f1e3b509
Dom Kapelewski <info@redfrogstudio.co.uk>
2018-02-16 06:24:35 +0000

    removed address field2

--- wp-content/themes/kickstarter/src/functions/woocommerce/checkout/checkout.php
+++ wp-content/themes/kickstarter/src/functions/woocommerce/checkout/checkout.php
@@ -5,8 +5,8 @@
  */
 
 function sv_require_second_street_address_field( $fields ) {
-    $fields['address_2']['required'] = true;
-    $fields['address_2']['label'] = esc_attr__( 'House/flat name or number', 'woocommerce' );
+    $fields['address_1']['label'] = esc_attr__( 'House/flat name or number, streen name', 'woocommerce' );
+    unset($fields['address_2']);
     return $fields;
 }
 add_filter( 'woocommerce_default_address_fields', 'sv_require_second_street_address_field' );
