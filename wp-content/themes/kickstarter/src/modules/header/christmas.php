a7449d848d63e873245a33fa02db8febb26db7bd
Dom Kapelewski <info@redfrogstudio.co.uk>
2019-12-16 06:40:28 +0000

    addec christmass message to the home page

--- wp-content/themes/kickstarter/src/modules/header/christmas.php
+++ wp-content/themes/kickstarter/src/modules/header/christmas.php
@@ -1,5 +1,5 @@
 <div class="snow">
   <div id="chrismtas-note">
-    <p>We will be open for business as normal up to and including Friday 21st December. Over the Christmas and New Year holidays our offices will be closed for phone orders and enquiries however on line website orders will be processed as normal. For cut of times and schedules please <a href="/contact/" title="<?php echo __('Contact Us', 'TEXT_DOAMIN') ?>">click here.</a></p>
+    <p>Over the Christmas and New Year period we will be shipping all on line orders as normal at 4pm each working day, orders can be placed over the holiday period on the website as normal. Our office will be closed for phone orders and support from 4pm Monday 23rd December until 9am Thursday 2nd January. We’d like to wish all our customers a merry Christmas and a happy New Year.</p>
   </div>
 </div>
