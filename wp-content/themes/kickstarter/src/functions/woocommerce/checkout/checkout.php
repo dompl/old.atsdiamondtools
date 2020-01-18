94ca2064ba32de5f18afe58ecab8d4b4ab34dfda
Dom <info@redfrogstudio.co.uk>
2020-01-18 08:24:19 +0000

    sorted address finder

--- wp-content/themes/kickstarter/src/functions/woocommerce/checkout/checkout.php
+++ wp-content/themes/kickstarter/src/functions/woocommerce/checkout/checkout.php
@@ -5,8 +5,8 @@
  */
 
 function sv_require_second_street_address_field($fields) {
-    $fields['address_1']['label'] = esc_attr__('House/flat name or number, Streen name', 'woocommerce');
-    $fields['address_1']['placeholder'] = esc_attr__('House/flat name or number, Streen name', 'woocommerce');
+    $fields['address_1']['label'] = esc_attr__('House/flat name or number, Street name', 'woocommerce');
+    $fields['address_1']['placeholder'] = esc_attr__('House/flat name or number, Street name', 'woocommerce');
     unset($fields['address_2']);
     return $fields;
 }
