d433a8f5f52e2e5cffe098591b1cc109f3a50b79
Dom <info@redfrogstudio.co.uk>
2020-09-05 06:28:29 +0100

    couple changes to css

--- wp-content/themes/kickstarter/src/functions/woocommerce/checkout/cart.php
+++ wp-content/themes/kickstarter/src/functions/woocommerce/checkout/cart.php
@@ -4,10 +4,10 @@
  *  ********************************************************
  */
 
-add_action('template_redirect', 'ats_checkout_redirects');
+add_action( 'template_redirect', 'ats_checkout_redirects' );
 
 function ats_checkout_redirects()
 {
-  add_action('woocommerce_after_cart', 'woocommerce_cross_sell_display', 10);
-  remove_action('woocommerce_cart_collaterals', 'woocommerce_cross_sell_display', 10);
+    add_action( 'woocommerce_after_cart', 'woocommerce_cross_sell_display', 10 );
+    remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display', 10 );
 }
