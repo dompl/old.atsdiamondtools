981a82404685c0d372dc2bbf3b837f30c9516221
Dom <info@redfrogstudio.co.uk>
2020-12-22 11:49:32 +0000

    added christmas note

--- wp-content/themes/kickstarter/src/modules/header/christmas.php
+++ wp-content/themes/kickstarter/src/modules/header/christmas.php
@@ -1,5 +1,5 @@
 <div class="snow">
-  <div id="chrismtas-note">
-    <p>Over the Christmas and New Year period we will be shipping all on line orders as normal at 4pm each working day, orders can be placed over the holiday period on the website as normal. Our office will be closed for phone orders and support from 4pm Monday 23rd December until 9am Thursday 2nd January. We’d like to wish all our customers a merry Christmas and a happy New Year.</p>
-  </div>
+	<div id="chrismtas-note">
+		<p>We will be open for on line orders throughout the Christmas period, our offices will not be manned for phone orders or enquiries. Orders will be processed as usual on a daily basis at 3pm for dispatch the same day on the 29th, 30th and 31st December. We re-open for normal business on Monday 4th January at 9am.</p>
+	</div>
 </div>
