75eca73b8f92766e4b7781375c42e31f1e5c4388
Dom <info@redfrogstudio.co.uk>
2021-02-03 15:25:54 +0000

    changed coronoa note

--- wp-content/themes/kickstarter/src/modules/header/corona.php
+++ wp-content/themes/kickstarter/src/modules/header/corona.php
@@ -1,5 +1,5 @@
 <div class="corona">
 	<div id="corona-note">
-		<p>We are open for business as usual. All orders placed before 4pm on our website are being processed and dispatched same day.<br />Please note that Royal Mail have changed the terms of their Special Delivery service. Orders are no longer guaranteed by 1PM - but they are being prioritised for next day delivery.</p>
+		<p>We will be implementing a 10% increase on all products from the 1<sup>st</sup> March 2021.<br />This is the first time in 7 years we have increased our prices and is due to unprecedented market conditions and increased shipping costs with our suppliers in both Europe and Asia.</p>
 	</div>
 </div>
