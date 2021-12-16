a98f448f8862abfeed7bf1e057d2f7b686e851c2
Dom <info@redfrogstudio.co.uk>
2021-12-16 09:40:16 +0000

    added christmass hours to header

--- wp-content/themes/kickstarter/src/modules/header/christmas.php
+++ wp-content/themes/kickstarter/src/modules/header/christmas.php
@@ -1,5 +1,5 @@
 <div class="snow">
-	<div id="chrismtas-note">
-		<p>We will be open for on line orders throughout the Christmas period, our offices will not be manned for phone orders or enquiries. Orders will be processed as usual on a daily basis at 3pm for dispatch the same day on the 29th, 30th and 31st December. We re-open for normal business on Monday 4th January at 9am.</p>
-	</div>
+    <div id="chrismtas-note">
+        <p>Our last working day for Christmas and the holiday period will be Thursday 23rd December. We will reopen on Tuesday 4th January. Website orders placed over the Christmas period will be shipped as normal on the 29th, 30th and 31st December but during the holiday period the office won’t be open to accept phone orders or provide advice.<br />We’d like to wish all our customers a Merry Christmas and a Happy New Year.</p>
+    </div>
 </div>
